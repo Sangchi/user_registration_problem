ddb0130c7556d8041603a3b02e3957eb99ef6615
Sangchi <prashnatchavan726386@gmail.com>
2024-08-12 12:15:53 +0530

    [prashant chavan]add: User need to enter a valid Last Name , Last name starts with Cap and has minimum 3 character

--- user_reg_problem.py
+++ user_reg_problem.py
@@ -1,9 +1,9 @@
 '''
 As a User need to
-enter a valid First
-Name
-- First name starts with Cap and has
-minimum 3 characters
+enter
+a valid Last
+Name - Last name starts with Cap and has
+minimum 3 character
 
 '''
 import re
@@ -18,15 +18,37 @@ def check_name(name):
     else:
         return False
 
-def main():
 
-    user_input=input("enter the first name:")
-    if check_name(user_input):
-        print(f"{user_input}  is valid")
+def check_last_name(input):
+
+    pattern=r"^[A-Z][a-z]{2,29}$"
+
+    if re.fullmatch(pattern,input):
+        return True
 
     else:
-        print(f"{user_input}  is not valid name")
+        return False
+
+def main():
+
+    user_input1=input("enter the first name:")
+    user_input2=input("enter your last name:")
+    user_input3=input("enter your email:")
+
+    if check_name(user_input1):
+        print(f"{user_input1}  is valid")
+
+    else:
+        print(f"{user_input1}  is not valid name")
+
+    if check_last_name(user_input2):
+        print(f"{user_input2} is valid name")
+
+    else:
+        print(f"{user_input2} is not valid name")
 
 
 if __name__=='__main__':
     main()
+
+
