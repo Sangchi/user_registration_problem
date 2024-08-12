1bf8a643951bcd6e4d8bcdd85284239bc33d953d
Sangchi <prashnatchavan726386@gmail.com>
2024-08-12 12:41:13 +0530

    [prashant chavan]add:User need to follow predefined Mobile Format

--- user_reg_problem.py
+++ user_reg_problem.py
@@ -1,11 +1,9 @@
 '''
 As a User need to
-enter
-a valid email
-- E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl
-& co) and 2 optional (xyz & in) with
-precise @ and . positions
-
+follow pre
+-defined
+Mobile Format - E.g. 91 9919819801 - Country code follow by space and 10
+digit number
 
 '''
 import re
@@ -40,11 +38,22 @@ def check_email(input):
     else:
         return False
 
+def check_mobile_number(input):
+
+    pattern=r"^91(\d{10})$"
+
+    if re.fullmatch(pattern,input):
+        return True
+
+    else:
+        return False
+
 def main():
 
     user_input1=input("enter the first name:")
     user_input2=input("enter your last name:")
     user_input3=input("enter your email:")
+    user_input4=input("enter you mobile number:")
 
     if check_name(user_input1):
         print(f"{user_input1}  is valid")
@@ -64,7 +73,12 @@ def main():
     else:
         print(f"{user_input3} is not valid Email")
 
+    if check_mobile_number(user_input4):
+        print(f"{user_input4} is valid mobile number")
+
+    else:
+        print(f"{user_input4} is not valid user mobile number")
+
 
 if __name__=='__main__':
     main()
-
